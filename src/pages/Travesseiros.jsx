16c559347b002769f44ccf2846294d160fa54fcf
JoaquimGCVS <jgui9292@gmail.com>
2025-07-10 13:44:03 -0300

    Pagina de travesseiros finalizada

--- src/pages/Travesseiros.jsx
+++ src/pages/Travesseiros.jsx
@@ -27,6 +27,11 @@ import fiberesp1 from "../assets/travesseiros/fiberpillowesp/1.webp";
 import fiberesp2 from "../assets/travesseiros/fiberpillowesp/2.webp";
 import fiberesp3 from "../assets/travesseiros/fiberpillowesp/3.webp";
 
+import meu1 from "../assets/travesseiros/meu/1.webp";
+import meu2 from "../assets/travesseiros/meu/2.webp";
+import meu3 from "../assets/travesseiros/meu/3.webp";
+import meu4 from "../assets/travesseiros/meu/4.webp";
+
 const imagensStillo = [
     {
         src: stillo1,
@@ -150,6 +155,33 @@ const imagensFiberpillowEspeciais = [
     }
 ]
 
+const imagensMeuPrimeiroOrthocrin = [
+    {
+        src: meu1,
+        alt: "Meu Primeiro Orthocrin Látex",
+        title: "Meu Primeiro Orthocrin Látex",
+        desc: "Travesseiro de 3 cm de altura. Ideal para o primeiro ano de vida. Medida: 30x40 cm."
+    },
+    {
+        src: meu2,
+        alt: "Meu Primeiro Orthocrin Visco",
+        title: "Meu Primeiro Orthocrin Visco",
+        desc: "Travesseiro de 4 cm de altura. Ideal para os primeiros 4 anos da criança. Medida: 35x55 cm."
+    },
+    {
+        src: meu3,
+        alt: "Meu Primeiro Orthocrin Fibra",
+        title: "Meu Primeiro Orthocrin Fibra",
+        desc: "Travesseiro de 10 cm de altura. Tecido 200 fios. Ideal para crianças de 3 a 7 anos. Medida: 40x60 cm."
+    },
+    {
+        src: meu4,
+        alt: "Meu Orthocrin Visco",
+        title: "Meu Orthocrin Visco",
+        desc: "Travesseiro de 7 cm de altura. Indicado para crianças de 7 a 12 anos. Medida: 48x68 cm."
+    }
+];
+
 const Travesseiros = () => {
     return (
         <div>
@@ -189,6 +221,13 @@ const Travesseiros = () => {
                 backgroundColor="#FFFFFF"
                 color="#80002D"
                 images={imagensFiberpillowEspeciais}
+            />
+            <Produtos
+                titulo="Meu Primeiro Orthocrin"
+                descCategoria="Acessórios exclusivos para crianças, desde o nascimento até a adolescência. Desenvolvidos com tecido antialérgico e Malha Belga Camomille, que possui fragrância natural para acalmar e relaxar. Respeitam a anatomia infantil e oferecem conforto seguro em todas as fases da infância."
+                backgroundColor="#FFFFFF"
+                color="#80002D"
+                images={imagensMeuPrimeiroOrthocrin}
             />
             <WhatsappButton/>
             <Footer/>
