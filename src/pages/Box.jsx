08d8e39fecbe5bc3cdada904bbbe487a888c49e4
JoaquimGCVS <jgui9292@gmail.com>
2025-07-29 15:22:11 -0300

    Substituindo imagens PNG por WebP para otimização de desempenho e atualizando o componente Box com novas propriedades de modal

--- src/pages/Box.jsx
+++ src/pages/Box.jsx
@@ -8,6 +8,13 @@ import box4 from "../assets/box/4.webp";
 import box5 from "../assets/box/5.webp";
 import box6 from "../assets/box/6.webp";
 
+import BB1 from "../assets/modais/box/BB1.webp";
+import BB2 from "../assets/modais/box/BB2.webp";
+import BB3 from "../assets/modais/box/BB3.webp";
+import BB4 from "../assets/modais/box/BB4.webp";
+import BB5 from "../assets/modais/box/BB5.webp";
+import BB6 from "../assets/modais/box/BB6.webp";
+
 import Seo from "../components/Seo/Seo";
 
 const imagensBox = [
@@ -15,37 +22,49 @@ const imagensBox = [
         src: box1,
         alt: "Box Baú",
         title: "Box Baú",
-        desc: "Base com acabamento em couro ecológico e mecanismo a gás. Ideal para armazenar e suportar grande peso com segurança."
+        desc: "Base com acabamento em couro ecológico e mecanismo a gás. Ideal para armazenar e suportar grande peso com segurança.",
+        srcModal: BB1,
+        descModal: ""
     },
     {
         src: box2,
         alt: "Bibox Spring",
         title: "Bibox Spring",
-        desc: "Produto 2 em 1 com cama auxiliar de molas ensacadas, ideal para quem busca conforto e praticidade sem abrir mão da qualidade no descanso diário."
+        desc: "Produto 2 em 1 com cama auxiliar de molas ensacadas, ideal para quem busca conforto e praticidade sem abrir mão da qualidade no descanso diário.",
+        srcModal: BB2,
+        descModal: ""
     },
     {
         src: box3,
         alt: "Bibox Orthocrin",
         title: "Bibox Orthocrin",
-        desc: "Cama auxiliar com espuma D28 e estrutura em madeira reflorestada. Solução prática e confortável para o dia a dia."
+        desc: "Cama auxiliar com espuma D28 e estrutura em madeira reflorestada. Solução prática e confortável para o dia a dia.",
+        srcModal: BB3,
+        descModal: ""
     },
     {
         src: box4,
         alt: "Bibox Eco",
         title: "Bibox Eco",
-        desc: "Produto 2 em 1 com espuma D28, estrutura resistente em madeira reflorestada e acabamento em couro ecológico."
+        desc: "Produto 2 em 1 com espuma D28, estrutura resistente em madeira reflorestada e acabamento em couro ecológico.",
+        srcModal: BB4,
+        descModal: ""
     },
     {
         src: box5,
         alt: "Box Eco",
         title: "Box Eco",
-        desc: "Base fixa resistente com estrutura em madeira reflorestada e acabamento em couro ecológico."
+        desc: "Base fixa resistente com estrutura em madeira reflorestada e acabamento em couro ecológico.",
+        srcModal: BB5,
+        descModal: ""
     },
     {
         src: box6,
         alt: "Box Sommier Plus Decor",
         title: "Box Sommier Plus Decor",
-        desc: "Base decorativa com estrutura em madeira reflorestada e tecido de decoração. Durável, segura e resistente."
+        desc: "Base decorativa com estrutura em madeira reflorestada e tecido de decoração. Durável, segura e resistente.",
+        srcModal: BB6,
+        descModal: ""
     }
 ];
 
